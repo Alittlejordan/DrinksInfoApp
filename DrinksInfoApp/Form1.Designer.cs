a738a16e981ae7f580590dc7fd8c1bce39c83fb3
JordanBeast <lilj9917@yahoo.com>
2023-11-08 18:10:15 -0600

    Add project files.

---
+++ DrinksInfoApp/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace DrinksInfoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.drinksComboBox = new System.Windows.Forms.ComboBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.typeTextBox = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.drinksLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.typeLabel = new System.Windows.Forms.Label();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.thumbnailPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.thumbnailPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.AccessibleName = "";
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Location = new System.Drawing.Point(160, 24);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(121, 21);
+            this.categoryComboBox.TabIndex = 0;
+            this.categoryComboBox.SelectedIndexChanged += new System.EventHandler(this.categoryComboBox_SelectedIndexChanged);
+            // 
+            // drinksComboBox
+            // 
+            this.drinksComboBox.FormattingEnabled = true;
+            this.drinksComboBox.Location = new System.Drawing.Point(383, 24);
+            this.drinksComboBox.Name = "drinksComboBox";
+            this.drinksComboBox.Size = new System.Drawing.Size(121, 21);
+            this.drinksComboBox.TabIndex = 1;
+            this.drinksComboBox.SelectedIndexChanged += new System.EventHandler(this.drinksComboBox_SelectedIndexChanged);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(49, 94);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // typeTextBox
+            // 
+            this.typeTextBox.Location = new System.Drawing.Point(49, 165);
+            this.typeTextBox.Name = "typeTextBox";
+            this.typeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.typeTextBox.TabIndex = 3;
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(49, 235);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.priceTextBox.TabIndex = 4;
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Location = new System.Drawing.Point(174, 8);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Padding = new System.Windows.Forms.Padding(0, 0, 50, 0);
+            this.categoryLabel.Size = new System.Drawing.Size(132, 13);
+            this.categoryLabel.TabIndex = 5;
+            this.categoryLabel.Text = "Select Category";
+            // 
+            // drinksLabel
+            // 
+            this.drinksLabel.AutoSize = true;
+            this.drinksLabel.Location = new System.Drawing.Point(412, 8);
+            this.drinksLabel.Name = "drinksLabel";
+            this.drinksLabel.Padding = new System.Windows.Forms.Padding(0, 0, 50, 0);
+            this.drinksLabel.Size = new System.Drawing.Size(115, 13);
+            this.drinksLabel.TabIndex = 6;
+            this.drinksLabel.Text = "Select Drink";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(79, 78);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 7;
+            this.nameLabel.Text = "Name";
+            // 
+            // typeLabel
+            // 
+            this.typeLabel.AutoSize = true;
+            this.typeLabel.Location = new System.Drawing.Point(82, 146);
+            this.typeLabel.Name = "typeLabel";
+            this.typeLabel.Size = new System.Drawing.Size(31, 13);
+            this.typeLabel.TabIndex = 8;
+            this.typeLabel.Text = "Type";
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Location = new System.Drawing.Point(82, 219);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(61, 13);
+            this.priceLabel.TabIndex = 9;
+            this.priceLabel.Text = "Instructions";
+            // 
+            // thumbnailPictureBox
+            // 
+            this.thumbnailPictureBox.Location = new System.Drawing.Point(226, 78);
+            this.thumbnailPictureBox.Name = "thumbnailPictureBox";
+            this.thumbnailPictureBox.Size = new System.Drawing.Size(360, 264);
+            this.thumbnailPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.thumbnailPictureBox.TabIndex = 10;
+            this.thumbnailPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.thumbnailPictureBox);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.typeLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.drinksLabel);
+            this.Controls.Add(this.categoryLabel);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.typeTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.drinksComboBox);
+            this.Controls.Add(this.categoryComboBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.thumbnailPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.ComboBox drinksComboBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox typeTextBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.Label drinksLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label typeLabel;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.PictureBox thumbnailPictureBox;
+    }
+}
+
---
+++ DrinksInfoApp/Form1.cs
@@ -0,0 +1,262 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace DrinksInfoApp
+{
+    public partial class Form1 : Form
+    {
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeUI();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Populate the category combo box
+            PopulateCategories();
+        }
+
+        private void InitializeUI()
+        {
+            // Initialize components
+
+            // Set form properties
+            this.Text = "Drink Menu";
+            categoryLabel.AutoSize = true;
+            drinksLabel.AutoSize = true;
+            nameLabel.AutoSize = true;
+            typeLabel.AutoSize = true;
+            priceLabel.AutoSize = true;
+
+            // Set combo box properties
+            categoryComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            drinksComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // Set text box properties
+            nameTextBox.ReadOnly = true;
+            typeTextBox.ReadOnly = true;
+            priceTextBox.ReadOnly = true;
+
+            // Add event handlers
+            categoryComboBox.SelectedIndexChanged += categoryComboBox_SelectedIndexChanged;
+            drinksComboBox.SelectedIndexChanged += drinksComboBox_SelectedIndexChanged;
+
+            // Set form load event
+            this.Load += Form1_Load;
+        }
+
+
+        private void categoryComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Populate the drinks combo box based on the selected category
+            PopulateDrinks(categoryComboBox.SelectedItem.ToString());
+        }
+
+        private async void drinksComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Fetch and display drink details
+            await DisplayDrinkDetails(drinksComboBox.SelectedItem.ToString());
+        }
+
+        private async void PopulateCategories()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Make the API request to get categories
+                    string url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list";
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    // Check if the request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read the response content as a string
+                        string jsonString = await response.Content.ReadAsStringAsync();
+
+                        // Parse the JSON response
+                        JObject jsonResponse = JObject.Parse(jsonString);
+
+                        // Extract the list of categories
+                        JArray categories = (JArray)jsonResponse["drinks"];
+
+                        // Populate the combo box with categories
+                        foreach (JToken category in categories)
+                        {
+                            categoryComboBox.Items.Add(category["strCategory"]);
+                        }
+
+                        // Select the first category by default
+                        if (categoryComboBox.Items.Count > 0)
+                        {
+                            categoryComboBox.SelectedIndex = 0;
+                        }
+                    }
+                    else
+                    {
+                        // Handle the error if the request was not successful
+                        MessageBox.Show($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during the request
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private async void PopulateDrinks(string category)
+        {
+            // Make API request to get drinks based on the selected category and populate the combo box
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Make the API request to get drinks based on the selected category
+                    string url = $"https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={category}";
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    // Check if the request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read the response content as a string
+                        string jsonString = await response.Content.ReadAsStringAsync();
+
+                        // Parse the JSON response
+                        JObject jsonResponse = JObject.Parse(jsonString);
+
+                        // Extract the list of drinks
+                        JArray drinks = (JArray)jsonResponse["drinks"];
+
+                        // Clear the existing items in the drinks combo box
+                        drinksComboBox.Items.Clear();
+
+                        // Populate the combo box with drinks
+                        foreach (JToken drink in drinks)
+                        {
+                            drinksComboBox.Items.Add(drink["strDrink"]);
+                        }
+
+                        // Select the first drink by default
+                        if (drinksComboBox.Items.Count > 0)
+                        {
+                            drinksComboBox.SelectedIndex = 0;
+                        }
+                    }
+                    else
+                    {
+                        // Handle the error if the request was not successful
+                        MessageBox.Show($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during the request
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+
+
+        private async Task DisplayDrinkDetails(string drinkName)
+        {
+            // Make API request to get drink details and display them in the text boxes
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Make the API request to get drink details based on the selected name
+                    string url = $"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={drinkName}";
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    // Check if the request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read the response content as a string
+                        string jsonString = await response.Content.ReadAsStringAsync();
+
+                        // Parse the JSON response
+                        JObject jsonResponse = JObject.Parse(jsonString);
+
+                        // Extract the list of drinks
+                        JArray drinks = (JArray)jsonResponse["drinks"];
+
+                        // Get the first drink (assuming there's only one with the given name)
+                        if (drinks.Count > 0)
+                        {
+                            JObject drink = (JObject)drinks[0];
+
+                            // Display drink details in the text boxes
+                            nameTextBox.Text = (string)drink["strDrink"];
+                            typeTextBox.Text = (string)drink["strCategory"];
+                            priceTextBox.Text = (string)drink["strInstructions"]; // Use any property you want
+
+                            // Add additional text boxes for other properties as needed
+
+                            // Load and display the thumbnail image
+                            string thumbnailUrl = (string)drink["strDrinkThumb"];
+                            LoadThumbnail(thumbnailUrl);
+                        }
+                    }
+                    else
+                    {
+                        // Handle the error if the request was not successful
+                        MessageBox.Show($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during the request
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private async void LoadThumbnail(string thumbnailUrl)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Make the API request to get the thumbnail image
+                    byte[] imageBytes = await client.GetByteArrayAsync(thumbnailUrl);
+
+                    // Convert the byte array to an Image object
+                    using (var ms = new System.IO.MemoryStream(imageBytes))
+                    {
+                        Image thumbnailImage = Image.FromStream(ms);
+
+                        // Set the thumbnail image in the PictureBox
+                        thumbnailPictureBox.Image = thumbnailImage;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during the request
+                MessageBox.Show($"An error occurred while loading the thumbnail: {ex.Message}");
+            }
+
+
+        }
+    }
+}
+
